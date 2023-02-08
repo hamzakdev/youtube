5769eed3d3b8767be52df4fbc8c6e13a8beed72c
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-02-08 07:58:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,21 @@ const path = require('path')
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:'https://fir-1ffa2.web.app', credentials:true}))
+// app.use(cors({origin:'https://fir-1ffa2.web.app', credentials:true}))
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type', 'Access-Control-Allow-Origin', 'Origin');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+    return;
+  }
+
+  next();
+});
 
 
 
